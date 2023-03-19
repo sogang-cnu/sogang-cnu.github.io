893c66f3c72fff65729f91a178520aceeb584c7d
DongZoo Lee <dongzoo@kodebox.io>
2023-03-19 21:38:42 +0900

    [WIP] Add HamburgerButton for mobile devices

--- src/common/ui/page/header/header.tsx
+++ src/common/ui/page/header/header.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import styled from "styled-components";
 
+import HamburgerButton from "./assets/hamburger-button.svg";
+import { HeaderItem } from "./header-item";
+
+const MobileMenuButton = styled.img.attrs({
+  src: HamburgerButton,
+  alt: "menu-open-icon",
+})``;
+
+const MobileDrawer = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  left: 0;
+  background: #312f2f;
+`;
 export const Header = styled(({ className }) => {
+  const [openDrawer, setOpenDrawer] = useState(false);
   return (
     <header className={className}>
+      <MobileMenuButton />
+      {createPortal(<MobileDrawer>123123</MobileDrawer>, document.body)}
       <HeaderItem label="HOME" active />
       <HeaderItem label="STUDIES" />
       <HeaderItem label="PROJECTS" />
@@ -16,4 +36,17 @@ export const Header = styled(({ className }) => {
 
   height: 10.4rem;
   padding: 3.6rem 0;
+
+  ${MobileMenuButton} {
+    display: none;
+  }
+
+  ${(props) => props.theme.device.large} {
+    ${MobileMenuButton} {
+      display: block;
+    }
+    ${HeaderItem} {
+      display: none;
+    }
+  }
 `;
